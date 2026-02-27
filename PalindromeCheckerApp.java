642e1b21b6f6254f059e9cf5f9c83c3501886939
ViShaLGaStU <vg7506@srmist.edu.in>
2026-02-27 10:24:52 +0530

    UC2

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,4 +1,4 @@
-public class UseCase1PalindromeCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Management System");
     }
